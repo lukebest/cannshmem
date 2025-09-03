a71ef0cf4efa1fe5e0c87dbaf127f0c9adffbf76
祝子祥 <zhuzixiang2@h-partners.com>
2025-09-03 08:50:53 +0000

    !312 ut * cleancode * ut * cmakelist * 忽略等号对齐 * cleancode * !301 update build.sh

--- tests/unittest/include/unittest/mem_host_get_and_put_test.cpp
+++ tests/unittest/include/unittest/mem_host_get_and_put_test.cpp
@@ -9,6 +9,7 @@
 #include "acl/acl.h"
 #include "shmem_api.h"
 #include "shmemi_host_common.h"
+#include "utils/func_type.h"
 
 using namespace std;
 
@@ -22,8 +23,10 @@ extern void shmem_barrier_all();
 
 class HostPutMemTest {
 public:
-    inline HostPutMemTest() {}
-    inline void Init(void* gva, void* dev, int64_t rank_, size_t element_size_)
+    inline HostPutMemTest()
+    {
+    }
+    inline void Init(void *gva, void *dev, int32_t rank_, size_t element_size_)
     {
         gva_gm = gva;
         dev_gm = dev;
@@ -35,75 +38,63 @@ public:
     {
         shmem_putmem(gva_gm, dev_gm, element_size, rank);
     }
+
 private:
     void *gva_gm;
     void *dev_gm;
 
-    int64_t rank;
+    int32_t rank;
     size_t element_size;
 };
 
-template<typename T>
 class HostGetMemTest {
 public:
-    inline HostGetMemTest() {}
-    inline void Init(T* gva, T* dev, int64_t rank_, size_t element_each_rank_, int64_t rank_size_)
+    inline HostGetMemTest()
+    {
+    }
+    inline void Init(uint8_t *gva, uint8_t *dev, int32_t rank_size_, size_t element_size_)
     {
         gva_gm = gva;
         dev_gm = dev;
 
-        rank = rank_;
-        element_each_rank = element_each_rank_;
         rank_size = rank_size_;
+        element_size = element_size_;
     }
     inline void Process(bool is_nbi = false)
     {
-        if (is_nbi) {
-            for (int i = 0; i < rank_size; i++) {
-                shmem_get_float_mem_nbi(dev_gm + 16 * i, gva_gm, 16, i % rank_size);
-                // how to sync in host process instead of barrier_all
-                // AscendC::SetFlag<AscendC::HardEvent::MTE3_MTE2>(EVENT_ID0);
-                // AscendC::WaitFlag<AscendC::HardEvent::MTE3_MTE2>(EVENT_ID0);
-            }
-        } else {
-            for (int i = 0; i < rank_size; i++) {
-                shmem_get_float_mem(dev_gm + 16 * i, gva_gm, 16, i % rank_size);
-            }
+        for (int i = 0; i < rank_size; i++) {
+            shmem_getmem((void *)(dev_gm + 16 * i), (void *)(gva_gm), 16, i % rank_size);
         }
     }
-private:
-    T *gva_gm;
-    T *dev_gm;
 
-    int64_t rank;
-    size_t element_each_rank;
-    int64_t rank_size;
+private:
+    uint8_t *gva_gm;
+    uint8_t *dev_gm;
+
+    int32_t rank_size;
+    size_t element_size;
 };
 
-
-void host_test_putmem(void* gva, void* dev, int64_t rank_, size_t element_size_)
+void host_test_putmem(void *gva, void *dev, int32_t rank_, size_t element_size_)
 {
     HostPutMemTest op;
     op.Init(gva, dev, rank_, element_size_);
     op.Process();
 }
 
-
-template<typename T>
-void host_test_getmem(T* gva, T* dev, int64_t rank_, size_t element_each_rank_, int64_t rank_size_)
+void host_test_getmem(uint8_t *gva, uint8_t *dev, int32_t rank_, size_t element_size_)
 {
-    HostGetMemTest<T> op;
-    op.Init(gva, dev, rank_, element_each_rank_, rank_size_);
+    HostGetMemTest op;
+    op.Init(gva, dev, rank_, element_size_);
     op.Process();
 }
 
-
 static void host_test_put_get_mem(int rank_id, int rank_size, uint64_t local_mem_size)
 {
-    int total_size = 16 * static_cast<int>(rank_size);
-    size_t input_size = total_size * sizeof(float);
+    int total_size = 16 * rank_size;
+    size_t input_size = total_size;
 
-    std::vector<float> input(total_size, 0);
+    std::vector<uint8_t> input(total_size, 0);
     for (int i = 0; i < 16; i++) {
         input[i] = (rank_id + 10);
     }
@@ -123,31 +114,29 @@ static void host_test_put_get_mem(int rank_id, int rank_size, uint64_t local_mem
     string p_name = "[Process " + to_string(rank_id) + "] ";
     std::cout << "After putmem:" << p_name;
     for (int i = 0; i < total_size; i++) {
-        std::cout << input[i] << " ";
+        std::cout << static_cast<int>(input[i]) << " ";
     }
     std::cout << std::endl;
-    host_test_getmem<float>((float*)ptr, (float*)dev_ptr, rank_id, 16, rank_size);
+    host_test_getmem((uint8_t *)ptr, (uint8_t *)dev_ptr, rank_size, 16);
     ASSERT_EQ(aclrtSynchronizeStream(shm::g_state_host.default_stream), 0);
-    sleep(2);
 
     ASSERT_EQ(aclrtMemcpy(input.data(), input_size, dev_ptr, input_size, ACL_MEMCPY_DEVICE_TO_HOST), 0);
 
     std::cout << "After getmem:" << p_name;
     for (int i = 0; i < total_size; i++) {
-        std::cout << input[i] << " ";
+        std::cout << static_cast<int>(input[i]) << " ";
     }
     std::cout << std::endl;
-    // for gtest
     int32_t flag = 0;
     for (int i = 0; i < total_size; i++) {
         int stage = i / 16;
-        if (input[i] != (stage + 10)) {
+        if (static_cast<int>(input[i]) != (stage + 10)) {
+            std::cout << "input:" << static_cast<int>(input[i]) << "stage:" << (stage + 10) << std::endl;
             flag = 1;
         }
     }
     ASSERT_EQ(flag, 0);
 }
-
 
 void test_host_shmem_putmem_and_getmem(int rank_id, int n_ranks, uint64_t local_mem_size)
 {
@@ -164,61 +153,71 @@ void test_host_shmem_putmem_and_getmem(int rank_id, int n_ranks, uint64_t local_
     }
 }
 
-void host_test_int32_g_and_p(int rank_id, int n_ranks, uint64_t local_mem_size)
-{
-    int total_size = static_cast<int>(n_ranks);
-    size_t input_size = total_size * sizeof(int);
-
-    int *ptr = static_cast<int*>(shmem_malloc(1024));
-    shmem_int32_p(ptr, rank_id + 10, rank_id);
-    ASSERT_EQ(aclrtSynchronizeStream(shm::g_state_host.default_stream), 0);
-    sleep(2);
-
-    int msg;
-    ASSERT_EQ(aclrtMemcpy(&msg, input_size, ptr, input_size, ACL_MEMCPY_DEVICE_TO_HOST), 0);
-
-    string p_name = "[Process " + to_string(rank_id) + "] ";
-    std::cout << p_name << ", putValue is " << msg << std::endl;
-    ASSERT_EQ(msg, rank_id + 10);
-
-    for (int i = 0; i < n_ranks; ++i) {
-        int getValue = shmem_int32_g(ptr, i);
-        std::cout << p_name << ", getValue is " << getValue << std::endl;
-        ASSERT_EQ(getValue, i + 10);
+#define TEST_P_AND_G(NAME, TYPE)                                                                 \
+    void host_test_##NAME##_g_and_p(int rank_id, int n_ranks, uint64_t local_mem_size)           \
+    {                                                                                            \
+        int total_size = n_ranks;                                                                \
+        size_t input_size = sizeof(TYPE);                                                        \
+                                                                                                 \
+        void *ptr = shmem_malloc(input_size);                                                    \
+        shmem_##NAME##_p((TYPE *)ptr, (TYPE)(rank_id + 10), rank_id);                            \
+        ASSERT_EQ(aclrtSynchronizeStream(shm::g_state_host.default_stream), 0);                  \
+        sleep(2);                                                                                \
+                                                                                                 \
+        TYPE msg;                                                                                \
+        ASSERT_EQ(aclrtMemcpy(&msg, input_size, ptr, input_size, ACL_MEMCPY_DEVICE_TO_HOST), 0); \
+                                                                                                 \
+        string p_name = "[Process " + to_string(rank_id) + "] ";                                 \
+        std::cout << p_name << ", putValue is " << msg << std::endl;                             \
+        ASSERT_EQ(msg, rank_id + 10);                                                            \
+                                                                                                 \
+        for (int i = 0; i < n_ranks; ++i) {                                                      \
+            int getValue = shmem_##NAME##_g((TYPE *)ptr, i);                                     \
+            std::cout << p_name << ", getValue is " << getValue << std::endl;                    \
+            ASSERT_EQ(getValue, i + 10);                                                         \
+        }                                                                                        \
     }
-}
+SHMEM_MEM_PUT_GET_FUNC(TEST_P_AND_G)
+#undef TEST_P_AND_G
 
-void test_host_shmem_int32_p_and_g(int rank_id, int n_ranks, uint64_t local_mem_size)
-{
-    int32_t device_id = rank_id % test_gnpu_num + test_first_npu;
-    aclrtStream stream;
-    test_init(rank_id, n_ranks, local_mem_size, &stream);
-    ASSERT_NE(stream, nullptr);
-
-    host_test_int32_g_and_p(rank_id, n_ranks, local_mem_size);
-    std::cout << "[TEST] begin to exit...... rank_id: " << rank_id << std::endl;
-    test_finalize(stream, device_id);
-    if (::testing::Test::HasFailure()) {
-        exit(1);
+#define TEST_P_AND_G(NAME, TYPE)                                                             \
+    void test_host_shmem_##NAME##_p_and_g(int rank_id, int n_ranks, uint64_t local_mem_size) \
+    {                                                                                        \
+        int32_t device_id = rank_id % test_gnpu_num + test_first_npu;                        \
+        aclrtStream stream;                                                                  \
+        test_init(rank_id, n_ranks, local_mem_size, &stream);                                \
+        ASSERT_NE(stream, nullptr);                                                          \
+                                                                                             \
+        host_test_##NAME##_g_and_p(rank_id, n_ranks, local_mem_size);                        \
+        std::cout << "[TEST] begin to exit...... rank_id: " << rank_id << std::endl;         \
+        test_finalize(stream, device_id);                                                    \
+        if (::testing::Test::HasFailure()) {                                                 \
+            exit(1);                                                                         \
+        }                                                                                    \
     }
-}
+SHMEM_MEM_PUT_GET_FUNC(TEST_P_AND_G)
+#undef TEST_P_AND_G
 
 TEST(TestMemHostApi, TestShmemMemGetAndPutMem)
 {
     const int process_count = test_gnpu_num;
     uint64_t local_mem_size = 1024UL * 1024UL * 1024;
     test_mutil_task(
-            [this](int rank_id, int n_ranks, uint64_t local_mem_size) {
-                test_host_shmem_putmem_and_getmem(rank_id, n_ranks, local_mem_size);
-            }, local_mem_size, process_count);
+        [this](int rank_id, int n_ranks, uint64_t local_mem_size) {
+            test_host_shmem_putmem_and_getmem(rank_id, n_ranks, local_mem_size);
+        },
+        local_mem_size, process_count);
 }
 
-TEST(TestMemHostApi, TestShmemMemGetAndPutInt)
-{
-    const int process_count = test_gnpu_num;
-    uint64_t local_mem_size = 1024UL * 1024UL * 1024;
-    test_mutil_task(
-            [this](int rank_id, int n_ranks, uint64_t local_mem_size) {
-                test_host_shmem_int32_p_and_g(rank_id, n_ranks, local_mem_size);
-            }, local_mem_size, process_count);
-}
+#define TEST_P_AND_G(NAME, TYPE)                                                    \
+    TEST(TestMemHostApi, TestShmem##NAME##MemGetAndPut)                             \
+    {                                                                               \
+        const int process_count = test_gnpu_num;                                    \
+        uint64_t local_mem_size = 1024UL * 1024UL * 1024;                           \
+        test_mutil_task(                                                            \
+            [this](int rank_id, int n_ranks, uint64_t local_mem_size) {             \
+                test_host_shmem_##NAME##_p_and_g(rank_id, n_ranks, local_mem_size); \
+            },                                                                      \
+            local_mem_size, process_count);                                         \
+    }
+SHMEM_MEM_PUT_GET_FUNC(TEST_P_AND_G)
--- tests/unittest/include/unittest/utils/func_type.h
+++ tests/unittest/include/unittest/utils/func_type.h
@@ -34,4 +34,17 @@
     FUNC(char, char);                \
     FUNC(bfloat16, bfloat16_t)
 
+#define SHMEM_MEM_PUT_GET_FUNC(FUNC) \
+    FUNC(float, float);       \
+    FUNC(double, double);     \
+    FUNC(int8, int8_t);       \
+    FUNC(int16, int16_t);     \
+    FUNC(int32, int32_t);     \
+    FUNC(int64, int64_t);     \
+    FUNC(uint8, uint8_t);     \
+    FUNC(uint16, uint16_t);   \
+    FUNC(uint32, uint32_t);   \
+    FUNC(uint64, uint64_t);   \
+    FUNC(char, char)
+
 #endif  // UT_FUNC_TYPE_H

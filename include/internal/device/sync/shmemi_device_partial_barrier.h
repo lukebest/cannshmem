ff0f9a7d0439ae72389ec3cf0bc10ec7e534e611
caixilong <caixilong2@h-partners.com>
2025-12-09 19:31:15 +0800

    bugfix

--- include/internal/device/sync/shmemi_device_partial_barrier.h
+++ include/internal/device/sync/shmemi_device_partial_barrier.h
@@ -110,7 +110,7 @@ SHMEM_DEVICE void shmemi_partial_barrier(shmem_team_t tid, __gm__ uint32_t *pes,
             auto s_base = (__gm__ int32_t *)s_sync;
             constexpr uint32_t clear_count = SHMEM_PARTIAL_BARRIER_PER_TEAM_SIZE / sizeof(int32_t);
             AscendC::LocalTensor<int32_t> ub_tensor_32;
-            ub_tensor_32.address_.logicPos = AscendC::TPosition::VECOUT;
+            ub_tensor_32.address_.logicPos = static_cast<uint8_t>(AscendC::TPosition::VECOUT);
             ub_tensor_32.address_.bufferAddr = SHMEM_INTERNAL_UB_BUF_START_ADDR;
             ub_tensor_32.address_.dataLen = clear_count;
             AscendC::Duplicate(ub_tensor_32, 0, clear_count);
